8182c777fc56d24e3dcef686963bb6bd2395e7bc
SONGHEE <pasong0511@gmail.com>
2023-07-02 18:52:10 +0900

    기능구현 완료

--- src/components/messages/MessageList.tsx
+++ src/components/messages/MessageList.tsx
@@ -1,3 +1,4 @@
+import React, { useRef, useEffect } from "react";
 import { LOGIN_USER } from "../../utils/api";
 import { $$Message } from "../../types/type";
 
@@ -5,24 +6,46 @@ interface IMessageDataProps {
     messages?: $$Message[];
 }
 
+interface IMessageProps {
+    message: $$Message;
+    isEned?: boolean;
+}
+
+function MessageItem({ message, isEned = false }: IMessageProps) {
+    const ref = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (isEned) {
+            //elem.scrollIntoView() - 특정 요소 위치로 화면 스크롤 이동하기
+            ref.current?.scrollIntoView({ behavior: "smooth" });
+        }
+    }, [isEned]);
+
+    return (
+        <div
+            ref={ref}
+            key={message.messageId}
+            className={`message ${
+                message.userId === LOGIN_USER.userId ? "message-own" : ""
+            }`}
+        >
+            <h5>
+                {message.username} (
+                {new Date(message.timestamp).toLocaleTimeString()}):
+            </h5>
+            <p>{message.message}</p>
+        </div>
+    );
+}
+
 function MessageList({ messages = [] }: IMessageDataProps) {
     return (
-        <div className="chatroom">
-            {messages.map((message) => (
-                <div
-                    key={message.messageId}
-                    className={`message ${
-                        message.userId === LOGIN_USER.userId
-                            ? "message-own"
-                            : ""
-                    }`}
-                >
-                    <h5>
-                        {message.username} (
-                        {new Date(message.timestamp).toLocaleTimeString()}):
-                    </h5>
-                    <p>{message.message}</p>
-                </div>
+        <div className="message-list-wraper">
+            {messages.map((message, idx) => (
+                <MessageItem
+                    message={message}
+                    isEned={idx === messages.length - 1} //마지막 인덱스인 경우
+                />
             ))}
         </div>
     );
